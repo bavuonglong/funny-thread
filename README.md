93e1e4c126900731740a17b273c5e29197dad9ea
Cuong Ngo <cuong.ngo@ascendcorp.com>
2018-10-10 14:55:46 +0700

    Add an example which cause deadlock

--- README.md
+++ README.md
@@ -14,10 +14,16 @@ Some way to make our program thread-safe
 
 ### Lock API
 1. Lock API provides all the functionality of synchronized keyword, with additional ways to create Condition for locking, timeout for waiting
-a lock. Some important method is *lock()*, *unLock()* and *tryLock()*. With synchronized keyword, we can end up with waiting infinite due to it does not provide time out mechanism.
+a lock. Some important method is *lock()*, *unLock()*, *tryLock()* and *lockInterruptibly()*. With synchronized keyword, we can end up with waiting infinite due to it does not provide time out mechanism.
 2. It is mandatory for using try finally block to make sure lock will be release, unlike synchronized.
 3. Synchronization can cover only method or block, whereas Lock can lock on one method and unlock on another method
 4. Synchronization does not provide fairness while Lock has, so that longest waiting thread gets the lock first
-5 We can create different condition lock, so that different thread can use different condition for locking
+5. We can create different condition lock, so that different thread can use different condition for locking
+
+### Dead lock
+Dead lock is a programming situation when two or more actions are waiting each other to finish
+To avoid dead lock, we should:
+1. Avoid nested locks
+2. If nested locks is necessary, then consider to lock different lock on the same order.
 
 
---
+++ src/main/java/com/codeko/funnythread/service/DeadlockDemo.java
@@ -0,0 +1,62 @@
+package com.codeko.funnythread.service;
+
+public class DeadlockDemo {
+    public static void main(String[] args) {
+        Object object1 = new Object();
+        Object object2 = new Object();
+        Object object3 = new Object();
+
+        Thread thread1 = new Thread(new SyncThread(object1, object2), "thread 1");
+        Thread thread2 = new Thread(new SyncThread(object2, object3), "thread 2");
+        Thread thread3 = new Thread(new SyncThread(object3, object1), "thread 3");
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+            thread3.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class SyncThread implements Runnable {
+
+    private final Object object1;
+    private final Object object2;
+
+    public SyncThread(Object object1, Object object2) {
+        this.object1 = object1;
+        this.object2 = object2;
+    }
+
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        System.out.println(name +" is acquiring lock on " + object1);
+        synchronized (object1) {
+            System.out.println(name + " acquired lock on " + object1);
+            doSomeHeavyWork();
+            System.out.println(name +" is acquiring lock on " + object2);
+            synchronized (object2) {
+                System.out.println(name + " acquired lock on " + object2);
+                doSomeHeavyWork();
+            }
+            System.out.println(name + " released lock on " + object2);
+        }
+        System.out.println(name + " released lock on " + object2);
+        System.out.println("Finish");
+    }
+
+    private void doSomeHeavyWork() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
